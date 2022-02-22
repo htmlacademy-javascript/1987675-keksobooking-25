8de09dd24eecb0f7eb7107b3039e251d5e5af589
Kirill <amerka606@gmail.com>
2022-02-22 16:02:48 +0600

    Исправил ошибку с неиспользуемыми функциями; изменил возвращаемые значения при неправильных аргументах; убрал переопределение переменных в функции

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 const getRandomInteger = (min, max) => {
   if (min < 0) {
-    return undefined;
+    return 'Диапазон должен быть положительным, включая ноль';
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min >= max) {
-    return undefined;
+  if (Math.ceil(min) >= Math.floor(max)) {
+    return 'Ближайшее к заданному начальному значению целое больше или равно конечному целому значению диапазона';
   }
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 };
 
-const getRandomFloat = (min, max, floating) => {
+const getRandomFloat = (min, max, floating = 0) => {
   if (min < 0) {
-    return undefined;
+    return 'Диапазон должен быть положительным, включая ноль';
   }
 
   if (min >= max) {
-    return undefined;
+    return 'Начальное значение диапазона больше или равно конечному';
   }
 
-  const randomFloat = Math.random() * (max - min) + min;
-  return Math.round(randomFloat * Math.pow(10, floating)) / Math.pow(10, floating);
+  const RANDOM_FLOAT = Math.random() * (max - min) + min;
+  return Math.round(RANDOM_FLOAT * Math.pow(10, floating)) / Math.pow(10, floating);
 };
+
+getRandomInteger(0.16, 1.921);
+getRandomFloat(0, 12, 3);
