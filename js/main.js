7a48f68e0846a9949608979df72ba74fa0dc68c8
Kirill <amerka606@gmail.com>
2022-02-23 01:35:45 +0600

    Добавил вывод ошибок в консоль; убрал лишние вызовы функций округления; разобрался с наименованием констант

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
+/* eslint-disable no-console */
+
 const getRandomInteger = (min, max) => {
   if (min < 0) {
-    return 'Диапазон должен быть положительным, включая ноль';
+    console.error('Диапазон должен быть положительным, включая ноль');
+    return null;
   }
 
-  if (Math.ceil(min) >= Math.floor(max)) {
-    return 'Ближайшее к заданному начальному значению целое больше или равно конечному целому значению диапазона';
+  const integerMin = Math.ceil(min);
+  const integerMax = Math.floor(max);
+
+  if (integerMin >= integerMax) {
+    console.error('Ближайшее к заданному начальному значению целое больше или равно конечному целому значению диапазона');
+    return null;
   }
 
-  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+  return Math.floor(Math.random() * (integerMax - integerMin + 1)) + integerMin;
 };
 
 const getRandomFloat = (min, max, floating = 0) => {
   if (min < 0) {
-    return 'Диапазон должен быть положительным, включая ноль';
+    console.error('Диапазон должен быть положительным, включая ноль');
+    return null;
   }
 
   if (min >= max) {
-    return 'Начальное значение диапазона больше или равно конечному';
+    console.error('Начальное значение диапазона больше или равно конечному');
+    return null;
   }
 
-  const RANDOM_FLOAT = Math.random() * (max - min) + min;
-  return Math.round(RANDOM_FLOAT * Math.pow(10, floating)) / Math.pow(10, floating);
+  const randomFloat = Math.random() * (max - min) + min;
+  return Math.round(randomFloat * Math.pow(10, floating)) / Math.pow(10, floating);
 };
 
-getRandomInteger(0.16, 1.921);
+getRandomInteger(0.7, 2.9);
 getRandomFloat(0, 12, 3);
