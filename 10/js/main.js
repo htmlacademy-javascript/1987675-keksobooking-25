940534232e916f28841fcb794762b255ec2a11d5
Keks <keks@htmlacademy.ru>
2022-03-24 09:38:04 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,9 @@
+import {validateOfferForm, setFormActivity} from './form.js';
+import {createMap} from './map.js';
+
+setFormActivity(false);
+
+const map = createMap();
+map.on('load', setFormActivity(true));
+
+validateOfferForm();
